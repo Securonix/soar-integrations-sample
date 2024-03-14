6d8776976d7f233a6933c8a569c693fc3fcff0d4
Sai K <ssai@securonix.com>
2024-03-14 16:31:17 +0530

    removed secret key

--- shodan/tests/test_shodan.py
+++ shodan/tests/test_shodan.py
@@ -7,7 +7,7 @@ pykson = Pykson()
 integration_class = Shodan()
 
 def test_ip_address():
-    req = '{"connectionParameters": {"server_url": "https://api.shodan.io/shodan", "api_token": "Nvz7ezByh2qGvBLdMfJuKgS51iFFJ4ns"}, "parameters": {"ip_addr": "1.1.1.1"}}'
+    req = '{"connectionParameters": {"server_url": "https://api.shodan.io/shodan", "api_token": "samplevalue"}, "parameters": {"ip_addr": "1.1.1.1"}}'
     req = pykson.from_json(req, RequestBody, True)
     resp = integration_class.ip_address(req)
     assert (resp is not None)
